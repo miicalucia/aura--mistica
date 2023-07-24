9529ca533f66e91c116a807650a0f56317a7911e
mlg <miicalucia95@gmail.com>
2023-07-24 12:57:55 -0300

    lista de productos

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ const sahumerio = new Producto(2, "Sahumerio", 500, "assets/sahumerios-producto.
 const bombita = new Producto(3, "Bombitas", 1500, "assets/bombas.jpg");
 const portaSahumerio = new Producto(4, "Porta Sahumerio", 1500, "assets/porta-sahumerio.jpg");
 const conos = new Producto(5, "Conos", 1500, "assets/conitos.jpg");
-const hornillos = new Producto(6, "Hornillos", 1500, "assets/hornillos.jpg");
+const hornillos = new Producto(6, "Hornillos", 1500, "assets/hornillos.jpeg");
 const velas = new Producto(7, "Velas", 1500, "assets/velas.jpg");
 
 const productos = [sahumador, sahumerio, bombita, portaSahumerio, conos, hornillos, velas];
@@ -22,21 +22,29 @@ let carrito = [];
 
 const contenedorProductos = document.getElementById("contenedorProductos");
 
+//Funcion para mostrar productos
+
 const mostrarProductos = () => {
     productos.forEach(producto => {
         const card = document.createElement("div");
-        card.classList.add("col-xl-3", "col-md-6", "col-xs-12");
+        card.classList.add("col-xl-3", "col-md-6", "col-sm-12");
         card.innerHTML = `
-                            <div class="card">
-                            <img class="card-img-tom imgProductos" src="${producto.img}" alt="${producto.nombre}">
-                            <div>
-                            <h3>${producto.nombre}</h3>
-                            <p>${producto.precio}</p>
-                            <button class="btn colorBoton" id="boton${producto.id}"> Agregar al Carrito </button>
-                            </div>
+                            <div clas="card">
+                                <img class="class-img-tom imgProductos" src="${producto.img}" alt"${producto.nombre}">
+                                <div class="card-body">
+                                    <h3>${producto.nombre}</h3>
+                                    <p>${producto.precio}</p>
+                                    <button class="btn colorBoton" id="boton${producto.id}">Agregar al carrito</button>
+                                </div>
                             </div>`
-        contenedorProductos.appendChild(card);
+    contenedorProductos.appendChild(card);
 
-        
+    //Agregar productos al carrito
+        const boton = document.getElementById(`boton${producto.id}`);
+        boton.addEventListener("click", () => {
+            agregarAlCarrito(producto.id);
+        })
     })
 }
+
+mostrarProductos();
